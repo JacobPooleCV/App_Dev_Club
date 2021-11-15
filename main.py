76fd7e650e1ec26f57724eeb398745e3b90e0240
natebomar <89210770+natebomar@users.noreply.github.com>
2021-11-14 18:27:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-x = 25
-if x > 10:
-  print("First")
-elif x > 30:
-  print("Second")
+# Import the os module
+import os
+
+print('What lab would you like to run? (0-10)')
+x = input()
+x = int(x)
+# Print the current working directory
+#print("Current working directory: {0}".format(os.getcwd()))
+
+# Change the current working directory
+if x == 0:
+  os.chdir('./000_Base_Folder')
+elif x == 1:
+  os.chdir('./001_Input_&_Variables')
+elif x ==2:
+  os.chdir('./002_Conditionals')
 else:
-  print("Third")
+  print('run again')
+
+# Print the current working directory
+#print("Current working directory: {0}".format(os.getcwd()))
+
+os.system('python main.py')
